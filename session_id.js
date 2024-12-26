c1bab756cc5331fabc3acd221b77e402bec8e69d
Waruni980 <lsadun444@gmail.com>
2024-12-26 20:56:35 +0530

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&KU0iAbrb#uQxUT51fMqJOjDcXntIDTuRWOaeWI5f_5D-HsYLwzhU",
 };
